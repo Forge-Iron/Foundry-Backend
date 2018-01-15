1bcda5bf07818ed54552c47748366875df0f0840
Samuel Resendez <saresend@usc.edu>
2018-01-14 21:09:31 -0800

    added 0.1 modesl

--- src/models.rs
+++ src/models.rs
@@ -1,14 +1,27 @@
 
 
 #[derive(Queryable)]
-pub struct Person {
+#[derive(Serialize)]
+pub struct GitPerson {
     id: i32,
     name: String,
     email: String,
 }
 
 #[derive(Queryable)]
-pub struct Issue {
+#[derive(Serialize)]
+pub struct GitIssue {
     id: i32,
     title: String,
+    body: String,
+    mentor: i32,
+}
+
+#[derive(Queryable)]
+#[derive(Serialize)]
+pub struct Resource {
+    id: i32,
+    url: String,
+    title: String,
+    issue: i32,
 }
